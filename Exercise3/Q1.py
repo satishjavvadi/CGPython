ddcd7c136f2cdba388c319c604c928bb669eb0fd
satishjavvadi <satish.talend9@gmail.com>
2022-02-19 16:13:43 +0530

    added

--- Exercise3/Q1.py
+++
@@ -1,3 +0,0 @@
-languages = "Java, Python, C++, Scala, C"
-lanList = languages.split(",")
-print(lanList)
--- Exercise3/Q5.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Enter the nth number:"))
-i = 1
-while n >= i:
-    print(i)
-    i += 1
